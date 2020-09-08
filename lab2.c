c9202353bc4b664466312a918c560d99286af40b
Russell <rdaswimmer@gmail.com>
2020-09-07 20:49:21 -0400

    added sum function

--- lab2.c
+++ lab2.c
@@ -4,9 +4,30 @@ Date: 9/7/2020
 Program Name: Lab2
 */
 
+#include <stdio.h>
 
+// function headers
+int sum(int n);
+double avg(int n);
 
 
 int main() {
-    return 0;
+    int n;
+    printf("Enter an int: ");
+    scanf("%d", &n);
+
+    printf("The sum of the first %d integers is %d\n", n, sum(n));
+}
+
+int sum(int n) {
+// calculaters the sum of the first n integers.
+    int i, sum = 0;
+    for(i = 0; i <= n; i++)
+        sum+=i;
+    return sum;
+}
+
+double avg(int n) {
+// calculates the average of the first n integers.
+
 }
